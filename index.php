6bc7aa5fba5057a4a01fc15365d8473985175e65
blanccarbone <contact@blanccarbone.com>
2014-12-18 00:26:34 -0500

    Commit initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
+    <title>php mysql pour les nuls</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
